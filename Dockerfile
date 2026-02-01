0c8ed80a45a860b9771ded8ad5605c8cd8d631da
Ritvik-Sapra <sapra.ritvik@outlook.com>
2026-02-01 16:03:03 +0530

    Adding docker compose, Dockerfile, and preparing for CI

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# ----- Stage 1: Build -----
+FROM maven:3.9.9-eclipse-temurin-17 AS builder
+WORKDIR /app
+
+# Copy only pom.xml first (better caching)
+COPY pom.xml .
+RUN mvn dependency:go-offline
+
+# Now copy source code
+COPY src ./src
+
+# Build the jar
+RUN mvn clean package -DskipTests
+
+# ----- Stage 2: Run -----
+FROM eclipse-temurin:17-jre
+WORKDIR /app
+
+# Copy only the built jar from builder stage
+COPY --from=builder /app/target/*.jar app.jar
+EXPOSE 8080
+
+# Run the app
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/ritvik/jobtracker/controller/JobApplicationController.java
+++ src/main/java/com/ritvik/jobtracker/controller/JobApplicationController.java
@@ -4,6 +4,9 @@ import com.ritvik.jobtracker.entity.JobApplication;
 import com.ritvik.jobtracker.repository.JobApplicationRepository;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 /*
     Before entering the controller, the HTTP request goes from Tomcat to the DispatcherServlet which then redirects it
     to the controller.
@@ -14,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/jobApplications")
 public class JobApplicationController {
+    private static final Logger log = LoggerFactory.getLogger(JobApplicationController.class);
     // We create an object named "repository" of type JobApplicationRepository. What does final mean?
     private final JobApplicationRepository repository;
     // This is a constructor to initialize the repository.
@@ -23,16 +27,19 @@ public class JobApplicationController {
     // This is for HTTP GET requests.
     @GetMapping
     public List<JobApplication> getAllJobApplications() {
+        log.info("Getting all job applications");
         return repository.findAll();
     }
     // This is for HTTP POST requests.
     @PostMapping
     public JobApplication createJobApplication(@RequestBody JobApplication job) {
+        log.info("Creating job application for company={}, role={}", job.getCompany(), job.getRole());
         return repository.save(job);
     }
     // This is for HTTP DELETE requests.
     @DeleteMapping("/{id}")
     public void deleteJobApplication(@PathVariable Long id) {
+        log.warn ("Deleting job application with id={}", id);
         repository.deleteById(id);
     }
 }
